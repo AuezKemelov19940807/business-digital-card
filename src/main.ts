7ed897fa66988c42d098894ab5a170e16c8fad6e
Auez <kemelove@bk.ru>
2026-08-31 02:50:02 +0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://business-digital-card-front.vercel.app',
+    ],
+    credentials: true,
+  });
+
   const port = Number(process.env.PORT) || 3000;
 
   app.useGlobalPipes(
