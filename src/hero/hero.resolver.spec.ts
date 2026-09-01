45d7fcfebe036866eea3e1d44aae0303441695d5
Auez <kemelove@bk.ru>
2026-09-01 20:25:24 +0500

    add vitest hero

--- src/hero/hero.resolver.spec.ts
+++ src/hero/hero.resolver.spec.ts
@@ -1,18 +1,96 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeroResolver } from './hero.resolver.js';
-
+import { HeroService } from './hero.service.js';
+import { Hero } from './types/hero.type.js';
 describe('HeroResolver', () => {
   let resolver: HeroResolver;
 
+  const heroServiceMock = {
+    get: vi.fn(),
+    update: vi.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HeroResolver],
+      providers: [
+        HeroResolver,
+
+        {
+          provide: HeroService,
+          useValue: heroServiceMock,
+        },
+      ],
     }).compile();
 
     resolver = module.get<HeroResolver>(HeroResolver);
+
+    vi.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should return hero', async () => {
+    const hero = {
+      id: 'main',
+      title: 'Full Stack Developer',
+      fullName: 'Auez Kemelov',
+      profession: 'Software Developer',
+      description: 'Experienced developer',
+      email: 'test@example.com',
+      location: 'Almaty',
+      isOpenToWork: true,
+      github: null,
+      linkedin: null,
+      telegram: null,
+      image: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    heroServiceMock.get.mockResolvedValue(hero);
+
+    const result = await resolver.hero();
+
+    expect(result).toEqual(hero);
+    expect(heroServiceMock.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should update hero', async () => {
+    const input = {
+      fullName: 'Auez Kemelov',
+    };
+
+    const updatedHero = {
+      id: 'main',
+      fullName: 'Auez Kemelov',
+    };
+
+    heroServiceMock.update.mockResolvedValue(updatedHero);
+
+    const result = await resolver.updateHero(input);
+
+    expect(result).toEqual(updatedHero);
+    expect(heroServiceMock.update).toHaveBeenCalledWith(input);
+  });
+
+  it('should return image URL if hero has image', () => {
+    const hero = {
+      id: 'main',
+      image: 'hero-image.jpg',
+    } as Hero;
+
+    const imageUrl = resolver.image(hero);
+
+    expect(imageUrl).toBe(`${process.env.API_URL}/api/files/${hero.image}`);
+  });
+
+  it('should return null if hero has no image', () => {
+    const hero = {
+      image: '',
+    } as Hero;
+
+    const imageUrl = resolver.image(hero);
+    expect(imageUrl).toBeNull();
   });
 });
--- src/hero/hero.service.spec.ts
+++ src/hero/hero.service.spec.ts
@@ -1,5 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeroService } from './hero.service.js';
+import { vi } from 'vitest';
+import { prisma } from '../lib/prisma.js';
+import { NotFoundException } from '@nestjs/common';
+vi.mock('../lib/prisma.js', () => ({
+  prisma: {
+    hero: {
+      findUnique: vi.fn(),
+      update: vi.fn(),
+    },
+  },
+}));
 
 describe('HeroService', () => {
   let service: HeroService;
@@ -10,9 +21,78 @@ describe('HeroService', () => {
     }).compile();
 
     service = module.get<HeroService>(HeroService);
+
+    vi.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return hero', async () => {
+    const hero = {
+      id: 'main',
+      title: 'Full Stack Developer',
+      fullName: 'Auez Kemelov',
+      profession: 'Software Developer',
+      description: 'Experienced developer',
+      email: 'test@example.com',
+      location: 'Almaty',
+      isOpenToWork: true,
+      github: null,
+      linkedin: null,
+      telegram: null,
+      image: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    vi.mocked(prisma.hero.findUnique).mockResolvedValue(hero);
+
+    const result = await service.get();
+    expect(result).toEqual(hero);
+  });
+
+  it('should throw NotFoundException if hero not found', async () => {
+    vi.mocked(prisma.hero.findUnique).mockResolvedValue(null);
+    await expect(service.get()).rejects.toThrow(
+      new NotFoundException('Hero not found'),
+    );
+  });
+
+  it('should update hero', async () => {
+    const input = {
+      fullName: 'Auez Kemelov',
+    };
+
+    const updatedHero = {
+      id: 'main',
+      title: 'Full Stack Developer',
+      fullName: 'Auez Kemelov',
+      profession: 'Software Developer',
+      description: 'Experienced developer',
+      email: 'test@example.com',
+      location: 'Almaty',
+      isOpenToWork: true,
+      github: null,
+      linkedin: null,
+      telegram: null,
+      image: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    vi.mocked(prisma.hero.update).mockResolvedValue(updatedHero);
+
+    const result = await service.update(input);
+
+    expect(result).toEqual(updatedHero);
+
+    expect(prisma.hero.update).toHaveBeenCalledWith({
+      where: {
+        id: 'main',
+      },
+      data: input,
+    });
   });
 });
